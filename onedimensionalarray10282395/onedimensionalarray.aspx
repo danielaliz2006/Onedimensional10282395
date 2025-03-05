ec3907f68c3c62378b4f20b4762c86446dbb08da
Daniela Aguilar <danielaaguilar2523@gmail.com>
2025-03-05 15:41:53 -0600

    Primer commit

---
+++ onedimensionalarray10282395/onedimensionalarray.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="onedimensionalarray.aspx.cs" Inherits="onedimensionalarray10282395.onedimensionalarray" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <br />
+    <asp:Label ID="Label1" runat="server" Text="Create Category Array"></asp:Label>
+    <br />
+    <asp:Button ID="btnCreateArray" runat="server" Onclick="btnCreateArray_Click" Text="Create Array" Width="130px" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ onedimensionalarray10282395/onedimensionalarray.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace onedimensionalarray10282395
+{
+    public partial class onedimensionalarray : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCreateArray_Click(object sender, EventArgs e)
+        {
+            // Declare and load the array
+            String[] strCategoryArray = new string[5];
+            strCategoryArray[0] = "Footwear - Men  's' ";
+            strCategoryArray[1] = "Clothing - Men  's' ";
+            strCategoryArray[2] = "Racquets - Women 's' ";
+            strCategoryArray[3] = "Footwear - Women 's' ";
+            strCategoryArray[4] = " Clothing - Women 's' ";
+
+            Session["strCategoryArray"] = strCategoryArray;
+
+
+            Response.Redirect("from2.aspx");
+        }
+
+    }
+}
+
